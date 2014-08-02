95544c0986c2109f37073f50d3642a9422f51268
Smattacus <Smattacus@gmail.com>
2014-08-01 20:41:50 -0500

    Added documentation string.

--- python/modules/spec.py
+++ python/modules/spec.py
@@ -13,6 +13,15 @@ def spec(x, dt):
     """
 Returns the normalized power spectrum (1 / sqrt(n) forward, 1 / sqrt(n)
 backwards). This is so that Perseval's rule may be used easily.
+Usage:
+    [f, g] = spec(x, dt)
+INPUTS:
+    x    - Array of data to perform the FFT on.
+    dt   - Time element between array elements. (Scalar). Used to generate the
+            frequency array.
+OUTPUTS:
+    f   - Frequency Array
+    g   - FFT array.
     """
     n = size(x)
     f0 = 1/abs(n * dt)
