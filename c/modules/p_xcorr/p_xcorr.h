6471158ae2293df40f619b47c9edc9fb028e5f90
Smattacus <Smattacus@gmail.com>
2014-08-06 17:59:27 -0500

    Fixed xcorr_unbiased prototype.

--- c/modules/p_xcorr/p_xcorr.h
+++ c/modules/p_xcorr/p_xcorr.h
@@ -3,4 +3,4 @@
 
 double xcorr_sum(double *, double *, int, int, int);
 void corr3_parallel(double *, double *, int, int, int, int, double *);
-double xcorr_unbiased(double *, double *, int, int, int, int);
+double xcorr_unbiased(double *, double *, int, int, int);
