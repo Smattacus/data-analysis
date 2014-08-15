631e79a39101796b74b959781ad8d08ccdae2a40
Sean Mattingly <smattingly@helium-login-0-2.local>
2014-08-14 19:43:26 -0500

    Fixed bug in calling mylib.corr3_parallel()

--- python/modules/threeptcorr_helpers.py
+++ python/modules/threeptcorr_helpers.py
@@ -23,17 +23,17 @@ def getAvgCorr(data, delta_t):
     arrlarge = c_double * ((2 * delta_t - 1)**2)
     runavgarr = np.zeros(( 2 * delta_t - 1) **2)
     bigarr = arrlarge()
+    #Do a little magic with zip()
     for d1, d2, a1, a2 in zip(data['D1_scale'], data['D2_scale'],
             data['ave1'].transpose(), data['ave2'].transpose()):
         f1 = d1.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
         f2 = d2.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+        #Use the stronger signal as f1 in f1 * f1(t - tau1) * f2(t - tau2)
         if a1 >= a2:
-            mylib.corr3_parallel(f1, f2, a1, a2, tau1, tau2, 1, bigarr)
+            mylib.corr3_parallel(f1, f2, size(d1), size(d2), tau1, tau2, 1, bigarr)
         else:
-            mylib.corr3_parallel(f2, f1, a2, a1, tau1, tau2, 1, bigarr)
+            mylib.corr3_parallel(f2, f1, size(d2), size(d1), tau1, tau2, 1, bigarr)
         runavgarr +=  runavgarr + bigarr
-        d1
-        a1
     runavgarr /= np.size(data['ave1'])
     runavgarr = np.ctypeslib.as_array(runavgarr).reshape(tau1,
             tau2).transpose()
