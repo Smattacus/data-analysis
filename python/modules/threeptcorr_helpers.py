196a30883b64cae522e77acb7adf2f825331ef4e
Smattacus <Smattacus@gmail.com>
2014-08-20 12:21:18 -0500

    Now runs with f1, f2 in certain orientation only once for a given input. Does not arbritrarily switch for given values of ave1, ave2.
    It counts the # of instances that ave1 > ave2. If ave1 > ave2 for half or more of the array, it does
    f1, f2; otherwise it will switch the order of f1 and f2 in calling the 3 point correlation function.

--- python/modules/threeptcorr_helpers.py
+++ python/modules/threeptcorr_helpers.py
@@ -23,13 +23,17 @@ def getAvgCorr(d, delta_t):
     arrlarge = c_double * ((2 * delta_t - 1)**2)
     runavgarr = np.zeros(( 2 * delta_t - 1) **2)
     bigarr = arrlarge()
+    avect = 0
+    for x, y in zip(d['ave1'].transpose(), d['ave2'].transpose()):
+        if x > y:
+            avect += 1
     #Do a little magic with zip()
     for d1, d2, a1, a2 in zip(d['D1_scale'], d['D2_scale'],
             d['ave1'].transpose(), d['ave2'].transpose()):
         f1 = d1.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
         f2 = d2.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
         #Use the stronger signal as f1 in f1 * f1(t - tau1) * f2(t - tau2)
-        if a1 >= a2:
+        if avect >= np.size(d['ave1'])/2:.
             mylib.corr3_parallel(f1, f2, np.size(d1), np.size(d2), tau1, tau2, 1, bigarr)
         else:
             mylib.corr3_parallel(f2, f1, np.size(d2), np.size(d1), tau1, tau2, 1, bigarr)
