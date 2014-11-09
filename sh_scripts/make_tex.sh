7df716fd8e02d538ba6aa68ac3609784e7f6e885
Sean W. M <smattacus@gmail.com>
2014-11-09 15:25:36 -0600

    Got working.

--- sh_scripts/make_tex.sh
+++ sh_scripts/make_tex.sh
@@ -1,12 +1,13 @@
-#!/user/bin/sh
+#!/usr/bin/sh
 #Remakes the latex file given as an argument with bibtex.
 
-bibname=echo $1 | sed -e "s/.tex//g"
-dviname=echo $1 | sed -e "s/.tex/.dvi/g"
-
-
+bibname=`echo $1 | sed -e "s/.tex//g"`
+dviname=`echo $1 | sed -e "s/.tex/.dvi/g"`
+echo $bibname
+echo $dviname
 latex $1
-bibtex bibname
+bibtex $bibname
 latex $1
 latex $1
 dvipdf $dviname
+echo "dvipdf $dviname"
