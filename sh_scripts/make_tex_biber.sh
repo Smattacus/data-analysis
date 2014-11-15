4d943259e66186a6e497dd7225430ac0db08ca99
Sean W. M <smattacus@gmail.com>
2014-11-15 12:47:28 -0600

    Initial commit.

---
+++ sh_scripts/make_tex_biber.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+#Remakes the latex file given as an argument with bibtex.
+
+bibname=`echo $1 | sed -e "s/.tex//g"`
+dviname=`echo $1 | sed -e "s/.tex/.dvi/g"`
+echo $bibname
+echo $dviname
+latex $1
+biber $bibname
+latex $1
+latex $1
+dvipdf $dviname
+echo "dvipdf $dviname"
